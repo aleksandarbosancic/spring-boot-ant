4290b2bbd0ad98694b4401e15910d778a6876086
Aleksandar Bosancic <aco.bosancic@gmail.com>
2017-04-26 22:31:08 +0200

    spring boot ant build base project

---
+++ Spring-Boot-Ant/src/spring/boot/ant/SpringBootAnt.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2017
+ * AlphaGate Automatisierungstechnik, Rankweil, Austria  * 
+ * @author      original: abo, last edit: $Author$
+ * @version     $Id$
+ */
+package spring.boot.ant;
+
+/**
+ *
+ * @author abosancic
+ */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAnt implements CommandLineRunner
+{
+
+    @Autowired
+    DemoService demoService;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringBootAnt.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception
+    {
+        demoService.print();
+    }
+}
